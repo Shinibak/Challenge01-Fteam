4f347415365f181438edf65510ff4bd316479ef7
Shinibak <felipehenriquelamas@gmail.com>
2022-12-02 21:59:11 -0300

    Primeiro commit

---
+++ bin/challenge01_fteam.dart
@@ -0,0 +1,5 @@
+import 'package:challenge01_fteam/challenge01_fteam.dart' as challenge01_fteam;
+
+void main(List<String> arguments) {
+  print('Hello world: ${challenge01_fteam.calculate()}!');
+}
---
+++ test/challenge01_fteam_test.dart
@@ -0,0 +1,8 @@
+import 'package:challenge01_fteam/challenge01_fteam.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
